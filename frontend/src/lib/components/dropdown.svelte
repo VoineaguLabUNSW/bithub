04ead9b536eb48ce20a0c0b5e3c03ef0e5a37bce
WalshKieran <k.walsh@unsw.edu.au>
2023-12-01 17:34:19 +1100

    Fixed text overflow

--- frontend/src/lib/components/dropdown.svelte
+++ frontend/src/lib/components/dropdown.svelte
@@ -13,12 +13,15 @@
     $: if(!open) filter = '';
   </script>
   
-<div class='w-48 flex flex-col items-stretch'>
+<div class='flex w-52 flex-col items-stretch'>
     <div class='text-sm ml-2'>{title}</div>
-    <Button disabled={disabled} color="light">{$selected?.name || placeholder}<i class='fas fa-angle-down pl-2 text-gray-200'/></Button>
-    <Dropdown placement='down' bind:open class="overflow-y-auto px-3 pb-3 text-sm h-44 divide-y divide-gray-100">
-    <div slot="header" class="p-3">
-        <Search autofocus placeholder='Filter...' bind:value={filter} size="md" />
+    <Button disabled={disabled} color="light">
+        <span class="overflow-x-hidden text-ellipsis">
+            {$selected?.name || placeholder}
+        </span><i class='fas fa-angle-down pl-2 text-gray-200'/></Button>
+    <Dropdown placement='down' headerClass="m-2" containerClass='w-60' bind:open class="overflow-y-auto px-3 pb-3 text-sm h-44 divide-y divide-gray-100">
+    <div slot="header">
+        <Search autofocus placeholder='Filter...' bind:value={filter} size="md"/>
     </div>
     {#each [...groups].map(([key, values]) => [key, values.filter(v => key.toLowerCase().includes(filter.toLowerCase()) || v.name.toLowerCase().includes(filter.toLowerCase()))]).filter(([_, values]) => values.length) as [key, values]}
         <div class='p-2 text-gray-400'>{key}</div>
@@ -27,9 +30,9 @@
                 {#if v.disabled}
                     <li class="rounded pl-4 p-2 bg-gray-100 dark:hover:bg-gray-600">{v.name}</li>
                 {:else if v.id == $selected?.id}
-                    <li class="rounded pl-4 p-2 bg-gray-100 dark:bg-gray-600">{v.name}</li>
+                    <li class="rounded pl-4 p-2 bg-gray-100 dark:bg-gray-600 overflow-x-hidden text-ellipsis" on:click={() => {open=false}}>{v.name}</li>
                 {:else}
-                    <li class="rounded pl-4 p-2 hover:bg-gray-100 dark:hover:bg-gray-600" on:click={() => {selected.set(v); open=false}}>{v.name}</li>
+                    <li class="rounded pl-4 p-2 hover:bg-gray-100 dark:hover:bg-gray-600  overflow-x-hidden text-ellipsis" on:click={() => {selected.set(v); open=false}}>{v.name}</li>
                 {/if}
             {/key}
         {/each}
